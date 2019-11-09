ef0ddc50e21c9aeb3591477d915387dd1c9928ce
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2019-11-09 10:42:59 -0300

    agrega distancia en km

--- main.js
+++ main.js
@@ -12,13 +12,14 @@ function mostrar() {
     let puntoDosX = Number($form.querySelector('#puntoDosX').value)
     let puntoDosY = Number($form.querySelector('#puntoDosY').value)
     let nodoResultado = document.querySelector('#resultado')
-    let distancia = Math.sqrt(Math.pow(puntoDosX - puntoUnoX, 2) + Math.pow(puntoDosY - puntoUnoY, 2));
+    let distanciaMetros = Math.sqrt(Math.pow(puntoDosX - puntoUnoX, 2) + Math.pow(puntoDosY - puntoUnoY, 2));
+    let distanciaKm = (distanciaMetros * 0.001).toFixed(2)
     let kmXhoraAmetrosXMin = 83.3333;
-    let tiempoCaminata = distancia / kmXhoraAmetrosXMin;
+    let tiempoCaminata = distanciaMetros / kmXhoraAmetrosXMin;
 
     if (tiempoCaminata > 0) {
         nodoResultado.className = ('valido')
-        nodoResultado.innerText = `La distancia entre el punto 1 y el punto 2 es de ${distancia.toFixed(2)} metros. \n Tardarias ${tiempoCaminata.toFixed(2)} minutos en llegar caminando a 5km/h.`
+        nodoResultado.innerText = `La distancia entre el punto 1 y el punto 2 es de ${distanciaMetros.toFixed(2)} metros (${distanciaKm}km) \n Tardarias ${tiempoCaminata.toFixed(2)} minutos en llegar caminando a 5km/h.`
     } else {
         nodoResultado.className = ('error')
         nodoResultado.innerText = 'Completa los campos correctamente'
